e4ba81e36e43c00f910dae643fd731dd93b57d4e
Carlson Santana Cruz <carlsonsantana@gmail.com>
2026-04-06 01:30:40 -0300

    Build GitHub Pages using GitHub Actions

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'js/script.[contenthash].js',
     clean: true,
   },
